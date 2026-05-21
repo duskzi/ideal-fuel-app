81184c56454c449f77c38e87e302a9a6d873414f
Kühl <felipekuhl321@gmail.com>
2026-05-21 12:12:28 -0300

    add: add basic folder structure and components like card and button

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+
+// 
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+   
+      <Text style={styles.title}>Álcool ou Gasolina?</Text>
+
+      <Text>Preço do Etanol:</Text>
+      <TextInput style={styles.input} placeholder='Digite o preço do etanol...' />
+
+      <Text>Preço da Gasolina:</Text>
+      <TextInput style={styles.input} placeholder='Digite o preço da gasolina...' />
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -15,6 +27,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    marginTop: 16,
+    marginBottom: 24,
+  },
+  input: {
+    padding: 8,
+    margin: 6,
+    color: 'grey',
+    backgroundColor: '#F2F2F2',
+    borderRadius: 16,
+  }
 });
